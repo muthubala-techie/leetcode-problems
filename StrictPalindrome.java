11fffb1df7912caff1388aa13460d0e6684e77fc
muthubala-techie <muthubala.techie@gmail.com>
2025-03-20 21:41:38 +0530

    feat : solve 2396. Strictly Palindromic Number

---
+++ StrictPalindrome.java
@@ -0,0 +1,8 @@
+public class StrictPalindrome{
+    public static void main(String[] args){
+        System.out.print(isStrictlyPalindromic(7));
+    }
+    static public boolean isStrictlyPalindromic(int n) {
+        return false;
+    }
+}
