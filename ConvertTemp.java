2a2d8d78daea3498588262c16df2f06b41c88bc6
muthubala-techie <muthubala.techie@gmail.com>
2025-03-21 13:03:35 +0530

    feat : solve 2469. Convert the Temperature

---
+++ ConvertTemp.java
@@ -0,0 +1,16 @@
+/**
+*   question : 2469. Convert the Temperature
+*/
+
+import java.util.Arrays;
+
+public class MaxAchive{
+    public static void main(String[] args){
+        System.out.print(Arrays.toString(convertTemperature(36.50)));
+    }
+    static public double[] convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32.00;
+        return new double[]{kelvin, fahrenheit};
+    }
+}
