a0b1c6d3dabae8479e029282e051198af9bd349a
muthubala-techie <muthubala.techie@gmail.com>
2025-04-03 10:37:08 +0530

    feat : solve 58. Add Digits

---
+++ AddDigits.java
@@ -0,0 +1,8 @@
+public class AddDigits{
+    public static void main(String[] args){
+        System.out.print(addDigits(38));
+    }
+    static public int addDigits(int num) {
+        return num == 0 ? 0 : 1 + (num - 1) % 9;
+    }
+}
