ba94d58fbf325c4660c02643d77810fb1a234082
muthubala-techie <muthubala.techie@gmail.com>
2025-04-03 10:32:17 +0530

    feat : solve 202. Happy Number

---
+++ HappyNumber.java
@@ -0,0 +1,27 @@
+/**
+*   question : 202. Happy Number
+*/
+
+import java.util.*;
+public class HappyNumber{
+    public static void main(String[] args){
+        System.out.print(isHappy(19));
+    }
+    static public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getSumOfSquares(n);
+        }
+        return n == 1;
+    }
+    static public int getSumOfSquares(int num) {
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
+}
